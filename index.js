2fc849f0eb868db02a9b0a7e0493a609242e2395
sarang87 <sarang87@vt.edu>
2020-08-30 17:15:20 -0400

    removed https from socketio require

--- index.js
+++ index.js
@@ -14,8 +14,7 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-var https = require('https').Server(app);
-var io = require('socket.io')(https); // api docs: http://socket.io
+var io = require('socket.io')(http); // api docs: http://socket.io
 var google = require('google'); // api docs: https://www.npmjs.com/package/google
 // var async = require('async');  //probbaly shouldn't need this now with promises
 var models = require('./models');
