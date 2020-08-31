d6d30befcc82bd5b2208d5fec5ff754cee2a7e2f
sarang87 <sarang87@vt.edu>
2020-08-30 20:44:46 -0400

    removed us strict

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ var google = require('google'); // api docs: https://www.npmjs.com/package/googl
 // var async = require('async');  //probbaly shouldn't need this now with promises
 var models = require('./models');
 var _ = require('lodash');
-'use strict' 
 var scholar = require('google-scholar');
 
 var searchScholar = true
